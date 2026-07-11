f003cae8364b028677fc5ada51a42fb1eeb087f2
AJ <46843456+amckenna41@users.noreply.github.com>
2026-07-11 23:59:53 +0100

    v1.1.0; bug fixes, new features, tests and docs, all in changelog

--- pysar_test.py
+++
@@ -1,8 +0,0 @@
-from pySAR.descriptors import Descriptors
-desc = Descriptors(config_file="thermostability.json")
-# all_desc = desc.get_all_descriptors()
-# all_desc.to_csv("prot_descriptors.csv")
-
-# encoding = Encoding(config_file=config_path)
-# desc_results_df = encoding.descriptor_encoding()
-# encoding.descriptor.moreaubroto_autocorrelation.columns
